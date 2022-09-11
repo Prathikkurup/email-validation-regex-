435bbd0b2c4884f0ab9fdad28e3d29abeefc2b38
prathik kurup <112079514+Prathikkurup@users.noreply.github.com>
2022-09-11 10:07:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re #importing regex module
+import re                                                 #importing regex module
 email_condition="^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"#made a variable,^ denotes "start with"+ denotes "addtion of condition "
                                                           # \backslash denotes 'search of a character' ? denotes " should be only 0 or 1"
 user_email=str(input("enter your email: "))               #taking input from user
@@ -6,4 +6,3 @@ if re.search(email_condition,user_email):                 #introduced both varia
     print("right email")
 else:
     print('wrong email')
-
